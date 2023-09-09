8bc4fb0f32b95c7df739bc12bf4c4438b7ffc487
iron-blooded <ayss2@mail.ru>
2023-09-10 08:33:18 +1200

    добавил экзекьютор для команды открытия списка материалов

--- src/main/java/org/hg/materials/Materials.java
+++ src/main/java/org/hg/materials/Materials.java
@@ -1,13 +1,16 @@
 package org.hg.materials;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.hg.materials.comands.open_material_list;
 
 public final class Materials extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+//        Bukkit.getServer().getPluginManager().registerEvents(this, this);
+        getCommand("open_material_list").setExecutor(new open_material_list(this));
     }
 
     @Override
