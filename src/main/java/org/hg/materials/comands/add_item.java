5b457d368689794493bb6a031bc1a2fde6856e63
iron-blooded <ayss2@mail.ru>
2023-09-10 17:43:31 +1200

    добавил работу слушателя

--- src/main/java/org/hg/materials/comands/add_item.java
+++ src/main/java/org/hg/materials/comands/add_item.java
@@ -17,7 +17,7 @@ public class add_item implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         Player player = (Player) commandSender;
-        player.openInventory(new AddItem().getInventory());
+        player.openInventory(new AddItem(plugin).getInventory());
         return true;
     }
 }
--- src/main/java/org/hg/materials/inventory_holders/AddItem.java
+++ src/main/java/org/hg/materials/inventory_holders/AddItem.java
@@ -1,21 +1,29 @@
 package org.hg.materials.inventory_holders;
 
+import com.google.gson.Gson;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.hg.materials.Database;
+import org.hg.materials.Materials;
+import org.hg.materials.attributes.Attributes;
 
 public class AddItem implements InventoryHolder, Listener {
     ItemStack target = new ItemStack(Material.TARGET);
     ItemStack accept = new ItemStack(Material.LIME_CONCRETE);
     ItemStack deny = new ItemStack(Material.RED_CONCRETE);
-    public AddItem(){
+    private static Materials plugin;
+    public AddItem(Materials plugin){
+        this.plugin = plugin;
         setDisplayName(target, ChatColor.YELLOW+"Положите в центр материал!");
         setDisplayName(accept, ChatColor.GREEN+"Подтвердить добавление материала");
         setDisplayName(deny, ChatColor.RED+"Отклонить добавление материала");
@@ -36,10 +44,14 @@ public class AddItem implements InventoryHolder, Listener {
         Inventory inventory = event.getClickedInventory();
         if (inventory.getHolder() instanceof AddItem){
             event.setCancelled(true);
-            switch (event.getSlot()){
-                case 13: inventory.setItem(13, event.getCursor());// установка предмета
-                case 38: event.getWhoClicked().closeInventory(); // отказ от добавления
-                case 42: ; // согласие на добавление
+            int slot = event.getSlot();
+            ItemStack item = inventory.getItem(13);
+            if (slot == 13) {
+                inventory.setItem(13, event.getCursor());
+            } else if (slot == 38) {
+                event.getWhoClicked().closeInventory(); // отказ от добавления
+            } else if (slot == 42 && !(item == null || item.getType().isAir())) {
+                plugin.database.addValue(item, new Attributes()); // согласие на добавление
             }
         }
     }
