e35e9509d09fbc53a21a4216e65f16c7efe555af
iron-blooded <ayss2@mail.ru>
2023-09-10 15:18:16 +1200

    + регистрация команды add_item

--- src/main/java/org/hg/materials/Materials.java
+++ src/main/java/org/hg/materials/Materials.java
@@ -1,16 +1,20 @@
 package org.hg.materials;
 
 import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.hg.materials.comands.add_item;
 import org.hg.materials.comands.open_material_list;
+import org.hg.materials.inventory_holders.AddItem;
 
-public final class Materials extends JavaPlugin {
+public final class Materials extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-//        Bukkit.getServer().getPluginManager().registerEvents(this, this);
+        Bukkit.getServer().getPluginManager().registerEvents(new AddItem(), this);
         getCommand("open_material_list").setExecutor(new open_material_list(this));
+        getCommand("add_item").setExecutor(new add_item(this));
     }
 
     @Override
